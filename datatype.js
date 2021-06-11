8147f4917f0c976df25e0eadbd6494736a9b07d4
MonishaSekarBE <monisha6664sa@gmail.com>
2021-06-11 12:11:57 +0530

    sample

---
+++ datatype.js
@@ -0,0 +1,20 @@
+//sign a form
+const uid ="abc1234";
+// uid="abc12345";
+var fullName = "monisha"
+var mailId = "moni@123gmail.com";
+var password = "12345";
+var courseCount = 0;
+var isloggedInFromGoogle =true;
+
+//fullName = prompt("enter your name");
+
+console.log(fullName);
+console.log(mailId);
+console.log("my uid is:",uid);
+
+console.log(`
+      with unique ID:${uid}
+      user is:${fullName}
+      and is email:${mailId}
+`);
---
+++ variable.js
@@ -0,0 +1,15 @@
+var fullName ="monisha";
+
+var course ="js";
+
+var isloggedin = true;
+
+var leggedCount = 34;
+
+console.log(leggedCount);
+console.log("leggedCount");
+
+var payentMode;
+paymentMode ="credit card";
+
+console.log(paymentMode);
