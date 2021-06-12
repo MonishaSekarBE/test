3ea70860a823c19d2725528b9360c95dce1b799c
MonishaSekarBE <monisha6664sa@gmail.com>
2021-06-12 19:54:13 +0530

    hello sample msg

---
+++ condition.js
@@ -0,0 +1,14 @@
+//login email google facebook
+
+var email = true;
+var google = false;
+var faceBook = true;
+
+if (email || faceBook || google){
+    console.log("login success");
+}
+if(email && google && faceBook){
+    console.log("allow user to make a purchase");
+} else {
+    console.log("you are not allwed")
+}
